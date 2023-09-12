97dbdc56bb9fe59fd29cabfad4153c6ba0f5af0f
Bahadir Sofuoglu <bahadirsofuoglu@gmail.com>
2023-09-12 18:31:36 +0300

    Icons added

---
+++ src/app/components/icons/MinusIcon.tsx
@@ -0,0 +1,20 @@
+const MinusIcon = () => {
+    return (
+        <svg
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke-width='1.5'
+            stroke='currentColor'
+            className='h-5 w-5'
+        >
+            <path
+                stroke-linecap='round'
+                stroke-linejoin='round'
+                d='M19.5 12h-15'
+            />
+        </svg>
+    );
+};
+
+export default MinusIcon;
