a9f40bd677c6b3521e7bd4185e4c89a1e18960da
Bahadir Sofuoglu <bahadirsofuoglu@gmail.com>
2023-09-16 02:19:54 +0300

    Nuxt config updated for cors

--- pages/api/generateJs.ts
+++ pages/api/generateJs.ts
@@ -13,6 +13,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (req.method !== 'POST') {
+        return res.status(405).end();
+    }
+
     const settings = req.body.fields;
 
     const baseUrl =
