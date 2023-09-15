9ea81f68d02ec1787ebd2500782047a190a31045
Bahadir Sofuoglu <bahadirsofuoglu@gmail.com>
2023-09-16 01:00:50 +0300

    codeBlock test updates

--- test/components/codeBlock.test.js
+++ test/components/codeBlock.test.js
@@ -1,23 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import CodeBlock from '../../components/CodeBlock';
-import { ToastProvider } from '../../context/toastContext';
-import { FieldSettingsProvider } from '../../context/fieldSettingsContext';
-describe('CodeBlock component', () => {
-    it('should render title, button and code correctly', () => {
-        render(
-            <ToastProvider>
-                <FieldSettingsProvider>
-                    <CodeBlock />
-                </FieldSettingsProvider>
-            </ToastProvider>
+import React from 'react';
+import { render, fireEvent, act } from '@testing-library/react';
+import CodeBlock from '../../components/CodeBlock'; // Uygun import yolu kullanın
+import { useToast } from '../../context/toastContext';
+
+jest.mock('../../context/toastContext', () => ({
+    useToast: jest.fn(() => ({
+        openToast: jest.fn(),
+    })),
+}));
+
+global.navigator.clipboard = {
+    writeText: jest.fn(),
+};
+
+describe('CodeBlock', () => {
+    it('should copy the code to clipboard and show success toast on button click', async () => {
+        const mockOpenToast = jest.fn();
+        useToast.mockImplementation(() => ({ openToast: mockOpenToast }));
+        global.navigator.clipboard.writeText.mockResolvedValueOnce(undefined);
+
+        const { getByRole } = render(<CodeBlock />);
+        const copyButton = getByRole('button', { name: /copy/i });
+
+        await act(async () => {
+            fireEvent.click(copyButton);
+        });
+
+        expect(global.navigator.clipboard.writeText).toHaveBeenCalled();
+        expect(mockOpenToast).toHaveBeenCalledWith(
+            'Code copied to clipboard',
+            'success'
+        );
+    });
+
+    it('should show error toast when copying to clipboard fails', async () => {
+        const mockOpenToast = jest.fn();
+        useToast.mockImplementation(() => ({ openToast: mockOpenToast }));
+        global.navigator.clipboard.writeText.mockRejectedValueOnce(
+            new Error('An error occurred')
         );
 
-        expect(screen.getByText('JavaScript Code')).toBeInTheDocument();
-        expect(screen.getByText('Copy')).toBeInTheDocument();
-        expect(
-            screen.getByText(
-                '<script src="https://popupgenarator.s3.amazonaws.com/popupgenarator.js"></script>'
-            )
-        ).toBeInTheDocument();
+        const { getByRole } = render(<CodeBlock />);
+        const copyButton = getByRole('button', { name: /copy/i });
+
+        await act(async () => {
+            fireEvent.click(copyButton);
+        });
+
+        expect(mockOpenToast).toHaveBeenCalledWith(
+            'Failed to copy code to clipboard'
+        );
     });
 });
